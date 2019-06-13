49d47bd437ff7b74b0c23fc2b4a529f8dbf4fe00
vasyavo <vasya0984@gmail.com>
2019-06-13 13:13:11 +0300

    Update modulec.umd.js

--- src/assets/modulec.umd.js
+++ src/assets/modulec.umd.js
@@ -1,20 +1,14 @@
 (function(global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined'
-    ? factory(
-        exports,
-        require('@angular/core'),
-        require('@angular/router'),
-        require('@clr/angular')
-      )
+    ? factory(exports, require('@angular/core'), require('@angular/router'))
     : typeof define === 'function' && define.amd
     ? define('modulec', [
         'exports',
         '@angular/core',
-        '@angular/router',
-        '@clr/angular'
+        '@angular/router'
       ], factory)
-    : factory((global.modulec = {}), global.ng.core, global.ng.router, null);
-})(this, function(exports, core, router, angular) {
+    : factory((global.modulec = {}), global.ng.core, global.ng.router);
+})(this, function(exports, core, router) {
   'use strict';
 
   /**
@@ -68,8 +62,7 @@
       args: [
         {
           selector: 'lib-modulec',
-          template:
-            '\n    <button type="button" class="btn btn-primary" (click)="basic = true">Open</button>\n\n    <clr-modal [(clrModalOpen)]="basic">\n    <h3 class="modal-title">I have a nice title</h3>\n    <div class="modal-body">\n        <p>But not much to say...</p>\n    </div>\n    <div class="modal-footer">\n        <button type="button" class="btn btn-outline" (click)="basic = false">Cancel</button>\n        <button type="button" class="btn btn-primary" (click)="basic = false">Ok</button>\n    </div>\n  </clr-modal>\n  ',
+          template: '<h2>Welcome to Module C</h2>',
           styles: []
         }
       ]
@@ -93,7 +86,6 @@
       args: [
         {
           imports: [
-            angular.ClarityModule,
             router.RouterModule.forChild([
               {
                 path: '',
