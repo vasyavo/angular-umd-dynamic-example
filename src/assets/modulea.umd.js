dfacf387fa8e58e3c53f871ab8bbf2b69e47caf3
vasyavo <vasya0984@gmail.com>
2019-07-04 19:59:25 +0300

    Update modulea.umd.js

--- src/assets/modulea.umd.js
+++ src/assets/modulea.umd.js
@@ -1,14 +1,24 @@
+import {TranslatePipe} from "../app/pipes/translate/translate.pipe";
+
 (function(global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined'
-    ? factory(exports, require('@angular/core'), require('@angular/router'), require('@ngrx/store'))
+    ? factory(
+        exports,
+        require('@angular/core'),
+        require('@angular/router'),
+        require('@ngrx/store'),
+        require('translate-pipe')
+      )
     : typeof define === 'function' && define.amd
     ? define('modulea', [
         'exports',
         '@angular/core',
         '@angular/router',
-        '@ngrx/store'
-      ], factory) : (f=>f)();
-})(this, function(exports, core, router, ngrxStore) {
+        '@ngrx/store',
+        'translate-pipe'
+      ], factory)
+    : (f => f)();
+})(this, function(exports, core, router, ngrxStore, TranslatePipe) {
   'use strict';
 
   /**
@@ -59,7 +69,7 @@
         {
           selector: 'lib-modulea',
           template:
-            '\n  <form>\n  <section class="form-block">\n      <label>Input Fields</label>\n      <div class="form-group">\n          <label for="formFields_1">Textbox [type="text"]</label>\n          <input type="text" id="formFields_1" placeholder="Email" size="35" disabled>\n          <input type="text" id="formFields_2" placeholder="Email 2" size="45">\n          <input type="text" id="formFields_3" placeholder="Email" size="25">\n          <input type="text" id="formFields_4" placeholder="Email 2">\n          <input type="text" id="formFields_5" placeholder="Email">\n          <input type="text" id="formFields_6" placeholder="Email 2">\n      </div>\n      <div class="form-group">\n          <label for="formFields_7">Textbox [type="password"]</label>\n          <input type="password" id="formFields_7" placeholder="Password">\n      </div>\n      <div class="form-group">\n          <label for="formFields_8">Textarea</label>\n          <textarea id="formFields_8" rows="5"></textarea>\n      </div>\n      <div class="form-group">\n          <label for="formFields_9">Textbox [type="url"]</label>\n          <input type="url" id="formFields_9" placeholder="https://vmware.github.io/clarity/" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_10">Textbox [type="tel"]</label>\n          <input type="tel" id="formFields_10" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_11">Textbox [type="date"]</label>\n          <input type="date" id="formFields_11" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_12">Textbox [type="datetime-local"]</label>\n          <input type="datetime-local" id="formFields_12" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_13">Textbox Readonly [type="text"]</label>\n          <input type="text" id="formFields_13" size="35" readonly value="Readonly Text">\n      </div>\n  </section>\n</form>  \n  \n  ',
+            '\n  <form>\n  <section class="form-block">\n      <label>Input Fields {{ \'LOGIN_COMPONENT__APP_CONTAINER\' | translate }}</label>\n      <div class="form-group">\n          <label for="formFields_1">Textbox [type="text"]</label>\n          <input type="text" id="formFields_1" placeholder="Email" size="35" disabled>\n          <input type="text" id="formFields_2" placeholder="Email 2" size="45">\n          <input type="text" id="formFields_3" placeholder="Email" size="25">\n          <input type="text" id="formFields_4" placeholder="Email 2">\n          <input type="text" id="formFields_5" placeholder="Email">\n          <input type="text" id="formFields_6" placeholder="Email 2">\n      </div>\n      <div class="form-group">\n          <label for="formFields_7">Textbox [type="password"]</label>\n          <input type="password" id="formFields_7" placeholder="Password">\n      </div>\n      <div class="form-group">\n          <label for="formFields_8">Textarea</label>\n          <textarea id="formFields_8" rows="5"></textarea>\n      </div>\n      <div class="form-group">\n          <label for="formFields_9">Textbox [type="url"]</label>\n          <input type="url" id="formFields_9" placeholder="https://vmware.github.io/clarity/" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_10">Textbox [type="tel"]</label>\n          <input type="tel" id="formFields_10" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_11">Textbox [type="date"]</label>\n          <input type="date" id="formFields_11" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_12">Textbox [type="datetime-local"]</label>\n          <input type="datetime-local" id="formFields_12" size="35">\n      </div>\n      <div class="form-group">\n          <label for="formFields_13">Textbox Readonly [type="text"]</label>\n          <input type="text" id="formFields_13" size="35" readonly value="Readonly Text">\n      </div>\n  </section>\n</form>  \n  \n  ',
           styles: []
         }
       ]
@@ -92,7 +102,7 @@
               }
             ])
           ],
-          declarations: [ModuleaComponent],
+          declarations: [ModuleaComponent, TranslatePipe],
           exports: [ModuleaComponent]
         }
       ]
